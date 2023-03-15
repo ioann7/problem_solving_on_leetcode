75b3e20e2864275baa19a2cbef09be97bd1a0be9
Ioann Chimrov <102480321+ioann7@users.noreply.github.com>
2023-03-15 13:12:10 +0300

    Solve 235. Lowest Common Ancestor of a BST

---
+++ 235_Lowest_Common_Ancestor_of_a_BST.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowest_common_ancestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val < p.val and root.val < q.val:
+                root = root.right
+            elif root.val > p.val and root.val > q.val:
+                root = root.left
+            else:
+                return root
