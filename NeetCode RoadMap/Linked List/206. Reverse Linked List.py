bbc511bf3822478ed606acf529915724a7133ce9
Ioann Chimrov <102480321+ioann7@users.noreply.github.com>
2023-03-24 10:46:41 +0300

    Update 206. Reverse Linked List.py

--- NeetCode RoadMap/Linked List/206. Reverse Linked List.py
+++ NeetCode RoadMap/Linked List/206. Reverse Linked List.py
@@ -7,7 +7,7 @@ class ListNode:
         self.next = next
 
 
-# Here is my first idea
+# Here is my first idea. Time complexity O(n); Space complexity O(1)
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         son = None
@@ -21,13 +21,13 @@ class Solution:
 
 
 # Here is neetcode idea which looks prettier to me
+# Using two pointers. Time complexity O(n); Space complexity O(1)
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
-        prev, curr = None, head
-
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        return prev
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        previous, current = None, head
+        while current:
+            temp = current.next
+            current.next = previous
+            previous = current
+            current = temp
+        return previous
