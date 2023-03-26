957329cf129304ae37d59e4889e9e62d9a58c7f4
Ioann Chimrov <102480321+ioann7@users.noreply.github.com>
2023-03-26 03:21:50 +0300

    Solve 20. Valid Parentheses

---
+++ NeetCode RoadMap/Stack/20. Valid Parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closed_brackets = {
+            ')': '(',
+            '}': '{',
+            ']': '[',
+        }
+        stack = []
+        for char in s:
+            if char not in closed_brackets:
+                stack.append(char)
+            else:
+                bracket = stack.pop() if stack else None
+                if closed_brackets[char] != bracket:
+                    return False
+        if stack:
+            return False
+        return True
