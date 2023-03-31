b1c72fc4cb86162b3eb5ca883f8767e73a5a4766
Ioann Chimrov <102480321+ioann7@users.noreply.github.com>
2023-03-31 21:38:40 +0300

    Update 235_Lowest_Common_Ancestor_of_a_BST.py

--- 235_Lowest_Common_Ancestor_of_a_BST.py
+++ 235_Lowest_Common_Ancestor_of_a_BST.py
@@ -8,10 +8,27 @@ class TreeNode:
         self.right = None
 
 
+# Time complexity O(logN). Space complexity O(1).
 class Solution:
     def lowest_common_ancestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         while root:
             if root.val < p.val and root.val < q.val:
+                root = root.right
+            elif root.val > p.val and root.val > q.val:
+                root = root.left
+            else:
+                return root
+
+
+# My first solution
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val == p.val:
+                return p
+            elif root.val == q.val:
+                return q
+            elif root.val < p.val and root.val < q.val:
                 root = root.right
             elif root.val > p.val and root.val > q.val:
                 root = root.left
