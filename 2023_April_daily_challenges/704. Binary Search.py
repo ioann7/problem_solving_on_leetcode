a19c4945f3c63ecdd90350e9fe31a29c0c8e149c
Ioann Chimrov <102480321+ioann7@users.noreply.github.com>
2023-04-02 10:14:55 +0300

    Solve 704. Binary Search

---
+++ 2023_April_daily_challenges/704. Binary Search.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/binary-search/
+
+from typing import List
+
+
+# Time complexity: O(logN). Space complexity O(1).
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if target < nums[mid]:
+                right = mid - 1
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                return mid
+        return -1
