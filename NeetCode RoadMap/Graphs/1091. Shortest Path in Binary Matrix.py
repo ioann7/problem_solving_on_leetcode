03d3fa3d6d91dea41af055ac71ee2e12a2a50b1a
Ioann Chimrov <102480321+ioann7@users.noreply.github.com>
2023-05-05 17:30:35 +0300

    Solve 1091. Shortest Path in Binary Matrix

---
+++ NeetCode RoadMap/Graphs/1091. Shortest Path in Binary Matrix.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/shortest-path-in-binary-matrix/
+
+# Time complexity O(ROWS * COLS). Space complexity O(ROWS * COLS) for visit.
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if grid[0][0] == 1:
+            return -1
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        visit = set()
+        queue = deque([(0, 0)])
+        length = 1
+        neighbors = [(0, -1), (0, 1), (-1, 0), (1, 0),
+                     (1, 1), (-1, -1), (-1, 1), (1, -1)]
+
+        while queue:
+            for _ in range(len(queue)):
+                row, col = queue.popleft()
+                if row == ROWS - 1 and col == COLS - 1:
+                    return length
+                for dr, dc in neighbors:
+                    is_out_of_bounds_or_visited = any((
+                        min(row + dr, col + dc) < 0,
+                        row + dr == ROWS,
+                        col + dc == COLS,
+                        (row + dr, col + dc) in visit,
+                    ))
+                    if not (is_out_of_bounds_or_visited or grid[row + dr][col + dc] == 1):
+                        visit.add((row + dr, col + dc))
+                        queue.append((row + dr, col + dc))
+            length += 1
+        return -1
