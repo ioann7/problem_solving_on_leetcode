e7b90e12d41c0b43a2418a6e735cf95f0684ced7
Ioann Chimrov <102480321+ioann7@users.noreply.github.com>
2023-05-11 22:51:51 +0300

    Solve 347. Top K Frequent Elements.py

--- NeetCode RoadMap/Arrays & Hashing/347. Top K Frequent Elements.py
+++ NeetCode RoadMap/Arrays & Hashing/347. Top K Frequent Elements.py
@@ -1,20 +1,15 @@
 # https://leetcode.com/problems/top-k-frequent-elements
 
-from typing import List
-from collections import Counter
-
-
+# Time complexity O(n). Space complexity O(n).
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [value for value, times in Counter(nums).most_common(k)]
-
-
-# without using collections.Counter
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = {}
-        for number in nums:
-            if number not in counter:
-                counter[number] = 0
-            counter[number] += 1
-        return sorted(counter.keys(), key=lambda key: counter[key], reverse=True)[:k]
+        counter = Counter(nums)
+        frequencies = [[] for _ in range(len(nums) + 1)]
+        for number, frequency in counter.items():
+            frequencies[frequency].append(number)
+        result = []
+        for frequency in reversed(frequencies):
+            for number in frequency:
+                result.append(number)
+                if len(result) == k:
+                    return result
