c48a82c78cf6a4ff86a61181f1db91be133fd5e3
Ioann Chimrov <102480321+ioann7@users.noreply.github.com>
2023-05-12 00:30:57 +0300

    Update solution 49 Group Anagrams

--- NeetCode RoadMap/Arrays & Hashing/49_Group_Anagrams.py
+++ NeetCode RoadMap/Arrays & Hashing/49_Group_Anagrams.py
@@ -1,27 +1,23 @@
 # https://leetcode.com/problems/group-anagrams/description/
 
+# Time complexity O(n*m) where n is avg len(str) and m is len(strs). Space complexity O(m). 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for s in strs:
+            count = [0] * 26    # a - z
+            for char in s:
+                count[ord(char) - ord('a')] += 1
+            result[tuple(count)].append(s)
+        return result.values()
 
-from typing import List
-from collections import defaultdict
 
-
+# My first solution
+# Time complexity O((n*log(n))*m) where n is avg len(str) and m is len(strs). Space complexity O(m).
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         result = defaultdict(list)
         for string in strs:
             key = str(sorted(string))
             result[key].append(string)
-        return result.values()
-
-
-# that solution without using defaultdict
-class Solution:
-    def group_anagrams(self, strs: List[str]) -> List[List[str]]:
-        result = {}
-        for string in strs:
-            key = str(sorted(string))
-            if key in result:
-                result[key].append(string)
-            else:
-                result[key] = [string]
         return result.values()
