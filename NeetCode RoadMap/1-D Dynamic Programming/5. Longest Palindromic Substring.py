550b3b0800f9c4011173f7ee5d201a36cbb35c8c
Ioann Chimrov <102480321+ioann7@users.noreply.github.com>
2023-05-19 23:47:33 +0300

    Solve 5. Longest Palindromic Substring

---
+++ NeetCode RoadMap/1-D Dynamic Programming/5. Longest Palindromic Substring.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/longest-palindromic-substring/
+
+# Time and space complexity O(n^2).
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        result = (0, 0) # (left, right)
+        for index in range(len(s)):
+            # odd length
+            result = max(
+                result,
+                self.find_palindrome(index, index, s),
+                key=lambda e: e[1] - e[0] + 1
+            )
+            # even length
+            result = max(
+                result,
+                self.find_palindrome(index, index + 1, s),
+                key=lambda e: e[1] - e[0] + 1
+            )
+        return s[result[0]:result[1] + 1]
+
+    def find_palindrome(self, left: int, right: int, s: str) -> Tuple[int, int]:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        # back to last good decision
+        left += 1
+        right -= 1
+        return left, right
