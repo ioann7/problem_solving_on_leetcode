31acd29098ef1a0e505e8e520af03adf8fcd3939
Ioann Chimrov <102480321+ioann7@users.noreply.github.com>
2023-05-25 12:46:01 +0300

    Solve 837. New 21 Game

---
+++ 2023_May_daily_challenges/837. New 21 Game.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/new-21-game/
+
+# Time complexity O(K + maxPts)
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        if k == 0 or k + maxPts <= n:
+            return 1.0
+        
+        window_sum = 0
+        for i in range(k, k + maxPts):
+            window_sum += 1 if i <= n else 0
+        
+        dp = {}
+        for i in range(k - 1, -1, -1):
+            dp[i] = window_sum / maxPts
+            remove = 0
+            if i + maxPts <= n:
+                remove = dp.get(i + maxPts, 1)
+            window_sum += dp[i] - remove
+        return dp[0]
